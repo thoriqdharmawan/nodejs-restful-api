1e3135f118353586ebe969e2f4802ec15a1e6884
thoriqdharmawan <thoriqd21@gmail.com>
2024-01-28 15:21:49 +0700

    feat: add search contacts

--- src/service/contact-service.js
+++ src/service/contact-service.js
@@ -1,6 +1,6 @@
 import { prismaClient } from "../application/database.js"
 import { ResponseError } from "../error/response-error.js"
-import { createContactValidation, getContactValidation, updateContactValidation } from "../validation/contact-validation.js"
+import { createContactValidation, getContactValidation, searchContactValidation, updateContactValidation } from "../validation/contact-validation.js"
 import { validate } from "../validation/validation.js"
 
 const create = async (user, request) => {
@@ -87,7 +87,7 @@ const remove = async (user, contactId) => {
     }
   })
 
-  if(contactCount !== 1) {
+  if (contactCount !== 1) {
     throw new ResponseError(404, 'Contacct is not found')
   }
 
@@ -98,9 +98,74 @@ const remove = async (user, contactId) => {
   })
 }
 
+const search = async (user, request) => {
+  request = validate(searchContactValidation, request)
+
+  const skip = ((request.page - 1) * request.size)
+
+  const filters = []
+
+  filters.push({
+    username: user.username
+  })
+
+  if (request.name) {
+    filters.push({
+      OR: [
+        {
+          first_name: {
+            contains: request.name
+          }
+        },
+        {
+          last_name: {
+            contains: request.name
+          }
+        }
+      ]
+    })
+  }
+
+  if (request.email) {
+    filters.push({
+      email: {
+        contains: request.email
+      }
+    })
+  }
+
+  if (request.phone) {
+    filters.push({
+      phone: {
+        contains: request.phone
+      }
+    })
+  }
+
+  const contacts = await prismaClient.contact.findMany({
+    where: { AND: filters },
+    take: request.size,
+    skip: skip
+  })
+
+  const totalContacts = await prismaClient.contact.count({
+    where: { AND: filters }
+  })
+
+  return {
+    data: contacts,
+    paging: {
+      page: request.page,
+      total_item: totalContacts,
+      total_page: Math.ceil(totalContacts / request.size)
+    }
+  }
+}
+
 export default {
   create,
   get,
   update,
   remove,
+  search,
 }
